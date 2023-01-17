3a908b772597310e42174137811f3d20426b2b8f
Enrique Marín <enriquemarin_sierra@hotmail.com>
2023-01-17 15:41:40 +0100

    fixed issue rusqlite lib

--- src/App.tsx
+++ src/App.tsx
@@ -52,4 +52,23 @@ export default App;
 /*
  * MODIFICAR ELEMENTOS PADRES EN BOOTSTRAP. VER:
  * https://stackoverflow.com/questions/42464888/how-do-i-change-the-background-color-of-the-body
+ *
+ * IMPORTANTE: SOLUCIONAR LOS PROBLEMAS DE COMPILACIÓN EN GITHUB ACTIONS. VER:
+ * COPIAR EL SCRIPT DE COMPILACIÓN QUE DA TAURI:
+ * https://github.com/tauri-apps/tauri-action#creating-a-release-and-uploading-the-tauri-bundles
+ * CAMBIAR ESTO EN tauri.confing.json:
+ * "beforeDevCommand": "pnpm dev",
+   "beforeBuildCommand": "pnpm build",
+ * Y PONER ESTO EN SU LUGAR:
+ * "beforeDevCommand": "npm run dev",
+   "beforeBuildCommand": "npm run build",
+ * DAR PERMISOS A GITHUB ACTIONS PARA ESCRITURA Y LECTURA (EN SETTING>ACTIONS>GENERAL>Workflow permissions>Read and write permissions):
+ * https://tauri.app/it/v1/guides/building/cross-platform/#github-environment-token
+ * PARA WINDOWS LA COMPILACIÓN FALLA CUANDO COMPILA LA BIBLIOTECA RUSQLITE. VER:
+ * https://github.com/dlang-community/d2sqlite3/issues/49
+ * LA SOLUCIÓN ES AÑADIR ESTO EN LA COMPILACIÓN DE BIBLIOTECAS DE RUST (archivo Cargo.toml). VER:
+ * https://github.com/rusqlite/rusqlite#user-content-notes-on-building-rusqlite-and-libsqlite3-sys
+ * [dependencies.rusqlite]
+   version = "0.28.0"
+   features = ["bundled"]
  */
